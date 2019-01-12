de95a4e53ae27eb1ca8508dc734b2f15c1ede229
Bogdan Chadkin <trysound@yandex.ru>
2019-01-13 02:44:37 +0300

    Fix webpack NODE_ENV elimination

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 // @flow
-const isProduction: boolean = process.env.NODE_ENV === 'production';
 
 export default function warning(condition: mixed, message: string) {
   // don't do anything in production
   // wrapping in production check for better dead code elimination
-  if (!isProduction) {
+  if (process.env.NODE_ENV !== 'production') {
     // condition passed: do not log
     if (condition) {
       return;
